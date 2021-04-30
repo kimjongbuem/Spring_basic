c365e529db40d6f3fb57ec549f74d03dc4d261b4
kimjongbuem <992mseven@naver.com>
2021-04-30 13:32:05 +0900

    <Customizing the nature of a bean> initailzingBean, DisposableBean을 이용해 init, destory 하는방법과 xml의 init-method, destroy-method로 설정하는방법! 특히 DisposableBean을 사용해서 빈을가져올려고 할때  ConfigureableAplicationContext를 통해 close하여 destory가 진행되는지 확인 최종파일은 xml설정만 되어있습니다.

--- src/main/java/kr/co/fastcampus/cli/ConnectionFactory.java
+++ src/main/java/kr/co/fastcampus/cli/ConnectionFactory.java
@@ -1,15 +1,22 @@
 package kr.co.fastcampus.cli;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@Slf4j
 public class ConnectionFactory {
 
     String driverName;
     String url;
     String password;
     String user;
+    @Getter
+    private Connection connection = null;
 
     public ConnectionFactory(String driverName, String url, String user, String password){
         this.driverName = driverName;
@@ -22,4 +29,14 @@ public class ConnectionFactory {
         Class.forName("org.h2.Driver");
         return DriverManager.getConnection(url, user, password);
     }
+
+    public void init() throws Exception {
+        log.info("init");
+        this.connection = createConnection();
+    }
+
+    public void destroy(){
+        log.info("destroy");
+    }
+
 }
--- src/main/java/kr/co/fastcampus/cli/Main.java
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -2,23 +2,33 @@ package kr.co.fastcampus.cli;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 @Slf4j
 class Main {
 	public static void main(String[] args) throws SQLException {
 		log.info("Hello world!!");
-		ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml" );
+//		ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml" );
 //		C a2 = context.getBean("C", C.class);
 //		C a1 = context.getBean("C", C.class);
 //		log.info("result = " + (a1 == a2));
 
-		Dao dao = context.getBean("dao", Dao.class);
-		dao.run();
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("dao.xml" );
+
+		ConnectionFactory factory = context.getBean(ConnectionFactory.class);
+		Connection connection = factory.getConnection();
+		if(connection!=null)
+		log.info("connection not null");
+
+//		log.info(">> 1 : " +  )
+//		context.close();
+
 
 	}
 }
