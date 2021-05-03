f96a4aa5fde8e6158e0e7395ec22381edcc6d20e
kimjongbuem <992mseven@naver.com>
2021-05-03 10:32:59 +0900

    Environment Abstraction
    -Dspring.profiles.active=mode
    
    xml 제거 모두 AppConfig에다 bean설정합니다.
    그래서 componentScan이 필요없어서 삭제합니다.
    공통으로 갖는 profiles | 를 통해 중복코드를 줄일수 있습니다.( 다른부분만 따로 빈설정 ) 하거나
    vm_mode 에서 의 프로필값에 따라 @PropertySource("classpath:application-${spring.profiles.active}.properties") 이런식으로 수정이 가능!

--- src/main/java/kr/co/fastcampus/cli/AppConfig.java
+++ src/main/java/kr/co/fastcampus/cli/AppConfig.java
@@ -1,17 +1,4 @@
 package kr.co.fastcampus.cli;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "kr.co.fastcampus.cli")
 public class AppConfig {
-    @Bean
-    public B b(){return new B();}
-
-    @Bean(initMethod = "init", destroyMethod = "destroy")
-    public A a(B b){
-        return new A(b);
-    }
 }
